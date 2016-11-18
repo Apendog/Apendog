4edfcefebd53d4a42b22cf789c81d00f07175ddb
jaytonb <jaytonbirch@gmail.com>
2016-11-18 05:17:34 -0700

    Continuation of the dog profile form to gather information of dog profile class. Still incomplete. Has more widgets and stuff.

--- app/src/main/java/alpha/com/apendog/loginActivity.java
+++ app/src/main/java/alpha/com/apendog/loginActivity.java
@@ -2,58 +2,108 @@ package alpha.com.apendog;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
-
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 
 public class loginActivity extends AppCompatActivity implements NumberPicker.OnValueChangeListener {
 
-    public NumberPicker np  = null;
-    public NumberPicker np2 = null;
+    int weight = 0;
+    public NumberPicker ageNumberPicker = null;
+    public NumberPicker monthYearPicker = null;
+    public SeekBar      weightSeeker    = null;
+    public TextView     weightLabel     = null;
+    public ImageView    dogPic          = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Getting picker widget from XML for age selector
+        // Getting picker widgets from XML for age and weight pickers
+            // number of years or months for age
+        ageNumberPicker = (NumberPicker) findViewById(R.id.numberPicker2);
+            // selects months or years
+        monthYearPicker = (NumberPicker) findViewById(R.id.numberPicker);
 
-        // This one is the number of years or months for age
-        np = (NumberPicker) findViewById(R.id.numberPicker2);
+        // seeks weight
+        weightSeeker = (SeekBar) findViewById(R.id.seekBar);
 
-        // This one selects months or years
-        np2 = (NumberPicker) findViewById(R.id.numberPicker);
+        // weight label
+        weightLabel = (TextView) findViewById(R.id.textView8);
+        weightLabel.setText(String.valueOf(weight));
 
+        //so that values in pickers cannot be modified by user
+        ageNumberPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+        monthYearPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 
-        //this makes it so values in pickers cannot be modified by user,
-        //    only selected
-        np.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
-        np2.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 
         // makes the picker cycle
-        np.setWrapSelectorWheel(true);
+        ageNumberPicker.setWrapSelectorWheel(true);
 
-        // We are populating the month/year picker with two values-"month" and "year"
-        np2.setMinValue(0);
+        // set dog pic and scale it
+        dogPic = (ImageView) findViewById(R.id.imageView);
+        /*dogPic.setScaleX(dogPic.getScaleX()/2);
+        dogPic.setScaleY(dogPic.getScaleY()/2);*/
 
-        np2.setMaxValue(1);
 
-        np2.setDisplayedValues(new String[]{"months", "years"});
 
-        np.setMinValue(0);
+        // to populate the month/year picker with two values-"month" and "year"
+        monthYearPicker.setMinValue(0);
+        monthYearPicker.setMaxValue(1);
+        monthYearPicker.setDisplayedValues(new String[]{"months", "years"});
 
-        np.setMaxValue(12);
+        // to populate the amount of month/years to 0-12 to start
+        ageNumberPicker.setMinValue(0);
+        ageNumberPicker.setMaxValue(12);
 
-        np2.setOnValueChangedListener(this);
+        // set max on seeker
+        weightSeeker.setMax(200);
+
+        // When the seeker is seeking run these
+        weightSeeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            // send the seeker value to label
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                weightLabel.setText(String.valueOf(progress) + " pounds");
+                float myFloat = (float)progress;
+                double myTestDouble = (myFloat/150) + 0.5;
+                float scaleFloat = (float)myTestDouble / 2;
+                dogPic.setScaleX(scaleFloat);
+                dogPic.setScaleY(scaleFloat);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        //set value change listener
+        monthYearPicker.setOnValueChangedListener(this);
     }
 
+    // if monthYearPicker value changes. This runs
     public void onValueChange(NumberPicker np2, int oldVal, int newVal) {
+
+        //if it is months. ageNumber picker is 0-12. Else 1-25.
         if (np2.getValue() == 0) {
-            np.setMinValue(0);
-            np.setMaxValue(12);
+            ageNumberPicker.setMinValue(0);
+            ageNumberPicker.setMaxValue(12);
         }
         else {
-            np.setMinValue(1);
-            np.setMaxValue(25);
+            ageNumberPicker.setMinValue(1);
+            ageNumberPicker.setMaxValue(25);
         }
     }
 
