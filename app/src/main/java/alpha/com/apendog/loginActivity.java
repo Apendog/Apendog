83dbddb2a35650ec9ef64e9119d29a789c0e3827
jaytonb <jaytonbirch@gmail.com>
2016-11-18 00:58:09 -0700

    Log in screen and part of dog profile creation. some responsive problems

--- app/src/main/java/alpha/com/apendog/loginActivity.java
+++ app/src/main/java/alpha/com/apendog/loginActivity.java
@@ -6,32 +6,55 @@ import android.widget.NumberPicker;
 
 
 
-public class loginActivity extends AppCompatActivity {
+public class loginActivity extends AppCompatActivity implements NumberPicker.OnValueChangeListener {
 
+    public NumberPicker np  = null;
+    public NumberPicker np2 = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Getting picker widget from XML
-       /* NumberPicker np = (NumberPicker) findViewById(R.id.numberPicker2);
+        // Getting picker widget from XML for age selector
 
-        //Populate NumberPicker values from minimum and maximum value range
-        //Set the minimum value of NumberPicker
-        np.setMinValue(0);
-        //Specify the maximum value/number of NumberPicker
-        np.setMaxValue(10);
-        //this method makes it so the user can't edit values of the picker
+        // This one is the number of years or months for age
+        np = (NumberPicker) findViewById(R.id.numberPicker2);
+
+        // This one selects months or years
+        np2 = (NumberPicker) findViewById(R.id.numberPicker);
+
+
+        //this makes it so values in pickers cannot be modified by user,
+        //    only selected
         np.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+        np2.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
+        // makes the picker cycle
         np.setWrapSelectorWheel(true);
 
-        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+        // We are populating the month/year picker with two values-"month" and "year"
+        np2.setMinValue(0);
 
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                picker.setValue((newVal < oldVal)?oldVal-5:oldVal+5);
-            }
+        np2.setMaxValue(1);
 
-        });*/
+        np2.setDisplayedValues(new String[]{"months", "years"});
+
+        np.setMinValue(0);
+
+        np.setMaxValue(12);
+
+        np2.setOnValueChangedListener(this);
     }
+
+    public void onValueChange(NumberPicker np2, int oldVal, int newVal) {
+        if (np2.getValue() == 0) {
+            np.setMinValue(0);
+            np.setMaxValue(12);
+        }
+        else {
+            np.setMinValue(1);
+            np.setMaxValue(25);
+        }
+    }
+
 }
