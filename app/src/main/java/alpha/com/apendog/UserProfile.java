cf5c079dffe408dd7aea5ff27c68f694a5d0b00c
Ed <ew1773@gmail.com>
2016-12-05 22:00:46 -0600

    MOnday night
    user and baseActivity classses built.

---
+++ app/src/main/java/alpha/com/apendog/UserProfile.java
@@ -0,0 +1,42 @@
+package alpha.com.apendog;
+
+/**
+ * Created by user on 11/28/2016.
+ */
+
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// [START blog_user_class]
+@IgnoreExtraProperties
+public class UserPorfile {
+
+    private String username = null;
+    private String email = null;
+    private boolean hasPetProfile = false;
+
+
+    public UserProfile() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public UserProfile(String username, String email, boolean hasPetProfile) {
+        this.username = username;
+        this.email = email;
+        this.hasPetProfile = hasPetProfile;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("username", username);
+        result.put("email", email);
+        result.put("hasPetProfile", hasPetProfile);
+
+        return result;
+    }
+
+}
---
+++ app/src/main/java/alpha/com/apendog/baseActivity.java
@@ -0,0 +1,8 @@
+package alpha.com.apendog;
+
+/**
+ * Created by user on 12/5/2016.
+ */
+
+public class baseActivity {
+}
